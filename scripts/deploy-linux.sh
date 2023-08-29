40fc9d39a424bac1d173ee72def11727eec36f24
MokkaCicc <mokkacicc@bave.me>
2023-08-29 17:20:11 +0200

    refactor: move deploy scripts to files

---
+++ scripts/deploy-linux.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+cp target/x86_64-unknown-linux-gnu/release/rummit .githooks/rummit-linux
+cp target/x86_64-pc-windows-gnu/release/rummit.exe .githooks/rummit-windows
---
+++ scripts/deploy-windows.bat
@@ -0,0 +1,2 @@
+copy target\x86_64-unknown-linux-gnu\release\rummit .githooks\rummit-linux
+copy target\x86_64-pc-windows-gnu\release\rummit.exe .githooks\rummit-windows
